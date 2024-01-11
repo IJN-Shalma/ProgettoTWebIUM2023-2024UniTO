dd1db78452e601a465345d22041464136baf9699
Yun Qing Zhou <53042240+SpacerCrownd@users.noreply.github.com>
2024-01-11 17:12:31 +0100

    route changes + jupyter notebook files

--- CapraraZhou/solution/ExpressMainServer/routes/mongo/gameEvents.js
+++ CapraraZhou/solution/ExpressMainServer/routes/mongo/gameEvents.js
@@ -46,4 +46,15 @@ router.get('/game/:gameId/player/:playerId', async (req, res) => {
     }
 });
 
+router.get('/club/:clubId', async (req, res) => {
+    try {
+        axios.get(`http://localhost:8082/game_events/club/${req.params.clubId}`)
+            .then((response) =>{
+                res.send(response.data);
+            });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
 module.exports = router;
